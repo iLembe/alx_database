6419aa12dfa69769b8fde5ed633594782dfba2dd
iLembe <mondli99zulu@gmail.com>
2023-10-09 19:09:07 +0200

    More Q5 Queries

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,13 +1,11 @@
 -- Use the specified database (replace 'your_database' with the actual database name)
-
 USE localhost;
 
--- Create the 'id_not_null' table if it does not already exist
-CREATE TABLE IF NOT EXISTS id_not_null (
-    -- Define the 'id' column as an integer with a NOT NULL constraint
-
+-- Create the 'unique_id' table if it doesn't already exist
+CREATE TABLE IF NOT EXISTS unique_id (
+    -- Define the 'id' column as an integer with a NOT NULL constraint and a unique constraint
     id INT NOT NULL DEFAULT 1,
-
+    
     -- Define the 'name' column as a variable character field with a maximum length of 256 characters
     name VARCHAR(256)
 );
