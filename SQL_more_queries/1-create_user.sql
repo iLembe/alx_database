3a0972cfd79c8fd18e3f1c46982374b40fd55bce
iLembe <mondli99zulu@gmail.com>
2023-10-09 14:55:34 +0200

    More Queries

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,12 +1,5 @@
-#!/bin/bash
-# Create the MySQL server user user_0d_1.
-# Check if the user already exists.
-user_exists=$(mysql -u root -p -e "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user='user_0d_1');")
-# If the user does not exist, create it.
-if [ "$user_exists" -eq 0 ]; then
-  mysql -u root -p -e "CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';"
-fi
-# Grant all privileges to the user.
-mysql -u root -p -e "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;"
-# Flush the privileges.
-mysql -u root -p -e "FLUSH PRIVILEGES;"
+-- Create or update user_0d_1 with all privileges
+GRANT ALL PRIVILEGES ON *.*
+ TO 'user_0d_1'@'localhost' 
+ IDENTIFIED BY 'user_0d_1_pwd' 
+ WITH GRANT OPTION;
